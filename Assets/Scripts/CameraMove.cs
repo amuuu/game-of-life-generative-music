29ac9e0e1dec112949ac56bf3d55c14e81f277f8
Amirreza Amouie <aamouie01@gmail.com>
2021-04-03 19:23:26 +0430

    add a quick bird's eye view control for camera

--- Assets/Scripts/CameraMove.cs
+++ Assets/Scripts/CameraMove.cs
@@ -5,7 +5,15 @@ using UnityEngine;
 public class CameraMove : MonoBehaviour
 {
 
-    public int rate; // 10
+    public int movementRate; // 10
+    public int rotationRate; // 4
+
+    private Vector3 rotation;
+
+    Vector3 lastAngles;
+    Vector3 lastPosition;
+
+
     void Start()
     {
         
@@ -16,27 +24,62 @@ public class CameraMove : MonoBehaviour
 
         if(Input.GetKey(KeyCode.D))
         {
-            transform.position = new Vector3(transform.position.x + rate, transform.position.y, transform.position.z);
+            transform.position = new Vector3(transform.position.x + movementRate, transform.position.y, transform.position.z);
         }
         if (Input.GetKey(KeyCode.A))
         {
-            transform.position = new Vector3(transform.position.x - rate, transform.position.y, transform.position.z);
+            transform.position = new Vector3(transform.position.x - movementRate, transform.position.y, transform.position.z);
         }
         if (Input.GetKey(KeyCode.W))
         {
-            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + rate);
+            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + movementRate);
         }
         if (Input.GetKey(KeyCode.S))
         {
-            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z - rate);
+            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z - movementRate);
         }
         if (Input.GetKey(KeyCode.G))
         {
-            transform.position = new Vector3(transform.position.x, transform.position.y - rate, transform.position.z);
+            transform.position = new Vector3(transform.position.x, transform.position.y - movementRate, transform.position.z);
         }
         if (Input.GetKey(KeyCode.H))
         {
-            transform.position = new Vector3(transform.position.x, transform.position.y + rate, transform.position.z);
+            transform.position = new Vector3(transform.position.x, transform.position.y + movementRate, transform.position.z);
+        }
+        if (Input.GetKey(KeyCode.T))
+        {
+            rotation = transform.eulerAngles;
+            rotation.x += rotationRate;
+            transform.eulerAngles = rotation;
+        }
+        if (Input.GetKey(KeyCode.Y))
+        {
+            rotation = transform.eulerAngles;
+            rotation.x -= rotationRate;
+            transform.eulerAngles = rotation;
+        }
+
+
+        // quick bird's eye view
+        if(Input.GetKeyDown(KeyCode.M))
+        {
+
+            lastPosition = transform.position;
+            lastAngles = transform.eulerAngles;
+            
+            rotation = transform.eulerAngles;
+            rotation.x = 90;
+            transform.eulerAngles = rotation;
+
+            transform.position = new Vector3(transform.position.x, 800, transform.position.z);
+        }
+
+        // quick bird's eye view REVERT
+        if (Input.GetKeyDown(KeyCode.N))
+        {
+            rotation = lastAngles;
+            transform.eulerAngles = rotation;
+            transform.position = lastPosition;
         }
     }
 }
