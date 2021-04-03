e4d1f78f372ae5449036b0d8329d311f50933742
Amirreza Amouie <aamouie01@gmail.com>
2021-04-04 02:14:07 +0430

    make the whole map array 3d, instead of 2d

--- Assets/Scripts/Generator.cs
+++ Assets/Scripts/Generator.cs
@@ -6,15 +6,17 @@ public class Generator : MonoBehaviour
 {
     public GameObject cubePrefab;
     public int maxMapSize; // 50
+    public int maxMapHeight; // 10
+
     public int padding; // 3
     public float cubeMargin; // 3
     public int numberOfParticles; //300
 
 
-    private bool[,] map;
-    private bool[,] newMap;
+    private bool[,,] map;
+    private bool[,,] newMap;
 
-    private GameObject[,] mapObjs;
+    private GameObject[,,] mapObjs;
 
 
     private float scale = 5;
@@ -29,13 +31,13 @@ public class Generator : MonoBehaviour
 
         presetBank = new PresetBank();
         
-        mapObjs = new GameObject[maxMapSize, maxMapSize];
-        newMap = new bool[maxMapSize, maxMapSize];
-        map = new bool[maxMapSize, maxMapSize];
+        mapObjs = new GameObject[maxMapSize, maxMapHeight, maxMapSize];
+        newMap = new bool[maxMapSize, maxMapHeight, maxMapSize];
+        map = new bool[maxMapSize, maxMapHeight, maxMapSize];
 
         GenerateRandomBlocks();
 
-        newMap = (bool[,]) map.Clone();
+        newMap = (bool[,,]) map.Clone();
 
         InitDisplay();
 
@@ -45,34 +47,38 @@ public class Generator : MonoBehaviour
     {
         for (int i = padding; i < maxMapSize - padding; i++)
         {
-            for (int k = padding; k < maxMapSize - padding; k++)
+            for (int j = 0; j < maxMapHeight; j++)
             {
-                newMap[i, k] = UpdateCellState(i, k);
+                for (int k = padding; k < maxMapSize - padding; k++)
+                {
+                    newMap[i, j, k] = UpdateCellState(i, j, k);
                 
-                UpdateDisplay(i, k);
+                    UpdateDisplay(i, j, k);
+                }
             }
         }
         
-        map = (bool[,]) newMap.Clone();
+        map = (bool[,,]) newMap.Clone();
     }
 
-    void UpdateDisplay(int x, int z)
+    void UpdateDisplay(int x, int y, int z)
     {
-        Destroy(mapObjs[x, z]);
+        Debug.Log("COORD: " + x + ", " + y + ", " + z);
+        Destroy(mapObjs[x, y, z]);
 
-        if (newMap[x, z])
+        if (newMap[x, y, z])
         {
 
-            mapObjs[x, z] = Instantiate(cubePrefab, new Vector3(x * blockDistance, 0, z * blockDistance), Quaternion.identity);
+            mapObjs[x, y, z] = Instantiate(cubePrefab, new Vector3(x * blockDistance, y * blockDistance, z * blockDistance), Quaternion.identity);
             
-            //mapObjs[x, z].GetComponent<MeshRenderer>().material.SetColor("_Color", new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f)));
+            //mapObjs[x, y, z].GetComponent<MeshRenderer>().material.SetColor("_Color", new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f)));
 
         }
     }
 
-    bool UpdateCellState(int x, int z)
+    bool UpdateCellState(int x, int y, int z)
     {
-        bool[] neighs = GetNeighbors(x, z);
+        bool[] neighs = GetNeighbors(x, y, z);
         int neighCounter = 0;
         
         foreach (bool item in neighs)
@@ -81,11 +87,11 @@ public class Generator : MonoBehaviour
                 neighCounter++;
         }
 
-        if (map[x, z] && (neighCounter == 2 || neighCounter == 3))
+        if (map[x, y, z] && (neighCounter == 2 || neighCounter == 3))
         {
             return true;
         }
-        else if(!map[x, z] && (neighCounter == 3))
+        else if(!map[x, y, z] && (neighCounter == 3))
         {
             return true;
         }
@@ -95,18 +101,18 @@ public class Generator : MonoBehaviour
         }
     }
 
-    bool[] GetNeighbors(int x, int z)
+    bool[] GetNeighbors(int x, int y, int z)
     {
         bool[] neighs = new bool[8];
 
-        neighs[0] = map[x, z + 1]; // u
-        neighs[1] = map[x + 1, z + 1]; // ur
-        neighs[2] = map[x + 1, z]; // r 
-        neighs[3] = map[x + 1, z - 1]; // dr 
-        neighs[4] = map[x, z - 1]; // d 
-        neighs[5] = map[x - 1, z - 1]; // dl
-        neighs[6] = map[x - 1, z]; // l 
-        neighs[7] = map[x - 1, z + 1]; // ul
+        neighs[0] = map[     x,   y,   z + 1 ]; // u
+        neighs[1] = map[ x + 1,   y,   z + 1 ]; // ur
+        neighs[2] = map[ x + 1,   y,   z     ]; // r 
+        neighs[3] = map[ x + 1,   y,   z - 1 ]; // dr 
+        neighs[4] = map[     x,   y,   z - 1 ]; // d 
+        neighs[5] = map[ x - 1,   y,   z - 1 ]; // dl
+        neighs[6] = map[ x - 1,   y,   z     ]; // l 
+        neighs[7] = map[ x - 1,   y,   z + 1 ]; // ul
 
         return neighs;
     }
@@ -115,9 +121,12 @@ public class Generator : MonoBehaviour
     {
         for (int i = padding; i < maxMapSize - padding; i++)
         {
-            for (int k = padding; k < maxMapSize - padding; k++)
+            for (int j = 0; i < maxMapHeight; j++)
             {
-                UpdateDisplay(i, k);
+                for (int k = padding; k < maxMapSize - padding; k++)
+                {
+                    UpdateDisplay(i, j, k);
+                }
             }
         }
     }
@@ -132,31 +141,29 @@ public class Generator : MonoBehaviour
     {
         //int a = Random.Range(maxMapSize/3, maxMapSize/2);
         int size = numberOfParticles;
-        List<(int,int)> already = new List<(int, int)>();
-        
+        List<(int, int, int)> already = new List<(int, int, int)>();
 
+        int x, y, z;
         for (int i=0; i< size; i++)
         {
-            int x = Random.Range(padding + 1, maxMapSize/2);
-            int z = Random.Range(padding + 1, maxMapSize/2);
-            while (_AlreadyExists(already, (x,z)))
-            {
+            do {
                 x = Random.Range(padding + 1, maxMapSize / 2);
+                y = Random.Range(0, maxMapHeight-1);
                 z = Random.Range(padding + 1, maxMapSize / 2);
-            }
-            
-            already.Add((x, z));
+            } while (_AlreadyExists(already, (x, y, z))) ;
+
+            already.Add((x, y, z));
 
             //Debug.Log("X, Z: " + x + "  " + z);
-            map[x, z] = true;
+            map[x, y, z] = true;
         }
-        newMap = (bool[,]) map.Clone();
+        newMap = (bool[,,]) map.Clone();
 
     }
 
-    bool _AlreadyExists(List<(int,int)> list, (int,int)target)
+    bool _AlreadyExists(List<(int, int, int)> list, (int, int, int) target)
     {
-        foreach ((int,int) item in list)
+        foreach ((int, int, int) item in list)
         {
             if (item.Equals(target))
                 return true;
@@ -174,38 +181,39 @@ public class Generator : MonoBehaviour
 
             if (prob > presetBank.blinkerPreset.probability)
             {
-                (int, int) coords = GenerateRandomCoordinate();
-                InsertPreset((Preset) presetBank.blinkerPreset, coords.Item1, coords.Item2);
+                (int, int, int) coords = GenerateRandomCoordinate();
+                InsertPreset((Preset) presetBank.blinkerPreset, coords.Item1, coords.Item2, coords.Item3);
             }
 
             if (prob > presetBank.toadPreset.probability)
             {
-                (int, int) coords = GenerateRandomCoordinate();
-                InsertPreset((Preset) presetBank.toadPreset, coords.Item1, coords.Item2);
+                (int, int, int) coords = GenerateRandomCoordinate();
+                InsertPreset((Preset) presetBank.toadPreset, coords.Item1, coords.Item2, coords.Item3);
 
             }
                 
         }
     }
 
-    void InsertPreset(Preset presetObject, int rx, int rz)
+    void InsertPreset(Preset presetObject, int rx, int ry, int rz)
     {
         
         for (int x = 0; x < presetObject.radius; x++)
         {
             for (int z = 0; z < presetObject.radius; z++)
             {
-                map[rx + x, rz + z] = presetObject.map[x, z];
+                map[rx + x, ry, rz + z] = presetObject.map[x, z];
             }
         }
     }
 
-    (int, int) GenerateRandomCoordinate()
+    (int, int, int) GenerateRandomCoordinate()
     {
         int randX = Random.Range(padding + presetBank.blinkerPreset.radius, maxMapSize - padding - presetBank.blinkerPreset.radius);
+        int randY = Random.Range(0, maxMapHeight - 1);
         int randZ = Random.Range(padding + presetBank.blinkerPreset.radius, maxMapSize - padding - presetBank.blinkerPreset.radius);
 
-        return (randX, randZ);
+        return (randX, randY, randZ);
     }
 }
 
