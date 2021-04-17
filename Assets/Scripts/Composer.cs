2195370c0b68e74d798c84601f65932fa9fd2d0f
Amirreza Amouie <aamouie01@gmail.com>
2021-04-18 01:43:40 +0430

    add new scripts for managing samples

---
+++ Assets/Scripts/Composer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Composer chooses sound samples based on user's settings such as scale type.
+
+public class Composer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
