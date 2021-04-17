88008e334cd394aa1789b3c5fe281091644a808f
Amirreza Amouie <aamouie01@gmail.com>
2021-04-18 02:36:50 +0430

    add code for loading files and instantiating prefabs

--- Assets/Scripts/Composer.cs
+++ Assets/Scripts/Composer.cs
@@ -1,13 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 // Composer chooses sound samples based on user's settings such as scale type.
 
-// File names: 36-pad-minilogue-1.wav   (note number - sound type - sound pack name - file name)
+// File names: A4-pad-1.wav   (note number - sound type - file name)
 
 public class Composer : MonoBehaviour
 {
+    public GameObject audioPrefab;
+    
     public int scaleType; // 1:minor / 2:major
     public int baseNote;
     
@@ -20,6 +23,7 @@ public class Composer : MonoBehaviour
     private int[] scale;
 
     private int[] allowedNotes;
+    int allowedNotesSize;
 
     private void Awake()
     {
@@ -32,21 +36,27 @@ public class Composer : MonoBehaviour
 
     void Start()
     {
+
+        
         switch (scaleType)
         {
             case 1: scale = minorScaleNotes; break;
             case 2: scale = majorScaleNotes; break;
         }
 
-        allowedNotes = new int [7];
+        allowedNotesSize = 7 * numOctaves;
+        allowedNotes = new int [allowedNotesSize];
+        
         CalcAllowedNotes();
+        
+
+        ScanDirectory();
     }
 
     // Update is called once per frame
     void Update()
     {
-        AudioSource a = new AudioSource();
-        a.clip = Resources.Load<AudioClip>("t");
+       
     }
 
     void InitScales()
@@ -70,11 +80,48 @@ public class Composer : MonoBehaviour
 
     void CalcAllowedNotes()
     {
-        for (int i = 0; i < 7; i++)
+        for (int i = 0; i < allowedNotesSize; i++)
         {
-            allowedNotes[i] = baseNote + scale[i];
+            allowedNotes[i] = baseNote + scale[i%7] + 12 * (i % 7);
         }
     }
 
+    void ScanDirectory()
+    {
+        string PATH = "Samples/";
+        
+        DirectoryInfo dir = new DirectoryInfo("Assets/Resources/"+PATH);
 
+        FileInfo[] info = dir.GetFiles("*.wav");
+
+        foreach (FileInfo f in info)
+        {
+            print("Found: " + f.Name);
+            string[] splittedName = f.Name.Split('-');
+
+            if (IsInAllowedNotes(NoteNameToNumber(splittedName[0])))
+            {
+                GameObject tmp = Instantiate(audioPrefab);
+                tmp.GetComponent<AudioSource>().clip = Resources.Load<AudioClip>(f.Name);
+            }
+        }
+
+    }
+
+    int NoteNameToNumber(string noteName)
+    {
+
+
+        return -1;
+    }
+
+    bool IsInAllowedNotes(int num)
+    {
+        for (int i = 0; i < allowedNotesSize; i++)
+        {
+            if (allowedNotes[i] == num)
+                return true;
+        }
+        return false;
+    }
 }
