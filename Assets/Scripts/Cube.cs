c1884765522317fdaf6a7493cccb09f267025553
Amirreza Amouie <aamouie01@gmail.com>
2021-04-20 03:24:51 +0430

    choose a random mass for each cube

--- Assets/Scripts/Cube.cs
+++ Assets/Scripts/Cube.cs
@@ -16,6 +16,9 @@ public class Cube : MonoBehaviour
     
     void Start()
     {
+        Rigidbody rb = GetComponent<Rigidbody>();
+        rb.mass = Random.Range(0.1f, 2f);
+
         GameObject[] tmp = GameObject.FindGameObjectsWithTag("Sounds");
         int index =  Random.Range(0, tmp.Length);
         
@@ -23,16 +26,20 @@ public class Cube : MonoBehaviour
 
         float distance = Vector3.Distance(cameraTransform.position, transform.position);
 
-        float vol= (float)(baseVol * distance / radius);
+        float vol = (float)(baseVol * distance / radius);
         audioSource.volume = vol;
         audioSource.pitch = Random.Range(0.2f, 1f);
 
         audioSource.PlayDelayed(Random.Range(0.05f,0.10f)*1.2f);//distance / 1000 + Random.Range(.2f,0.5f));
         //audioSource.Play();
+
+
+      
     }
     
     void Update()
     {
         
     }
+
 }
