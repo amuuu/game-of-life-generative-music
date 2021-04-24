712370aa0b90946bf9a6b57fd4e16f557cdb5153
Amirreza Amouie <aamouie01@gmail.com>
2021-04-24 23:20:13 +0430

    rewrite the whole composer (except chords)

--- Assets/Scripts/Composer.cs
+++ Assets/Scripts/Composer.cs
@@ -1,132 +1,74 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using UnityEngine;
 
-
-///define NUM_SCALE_NOTES = 7;
-
-// Composer chooses sound samples based on user's settings such as scale type.
-
-// File names: As4-pad-1.wav   (note name - sound type - file name)
-
-public class Composer : MonoBehaviour
+public struct Settings
 {
-    public GameObject audioPrefab;
-    
     public int scaleType; // 1:minor / 2:major
     public int baseNote; // c2: 36 c8: 88
     public int numOctaves; // 3
+}
+public class Composer : MonoBehaviour
+{
 
+    public GameObject audioPrefab;
+
+    public int scaleType; // 1:minor / 2:major
+    public int baseNote; // c2: 36 c8: 88
+    public int numOctaves; // 3
+    public bool isChordMode = false;
+
+    private string samplesRootPath = "Samples/";
+
+    ///////////////
     private ComposerController controller;
-
-    private List<int> loadedNotes;
-    private Dictionary<GameObject, int> soundObjectsDict;
-
-
-    private float timeToGo;
-
+    private DirectoryInfo dir;
+    FileInfo[] info;
+    private Note[] nextChord;
 
     void Start()
     {
-        soundObjectsDict = new Dictionary<GameObject, int>();
+        controller = new ComposerController(scaleType, baseNote, numOctaves);
 
-
-        timeToGo = Time.fixedTime + 1;
-
-        loadedNotes = new List<int>();
-
-        controller = new ComposerController(baseNote, scaleType, numOctaves);
-        
+        // scan the directory and load all the sounds in the scale
         ScanDirectory();
-
-        //StartCoroutine(ChordChangeCoroutine());
-
     }
 
-    private void FixedUpdate()
+    void FixedUpdate()
     {
-        if (Time.fixedTime >= timeToGo)
+        // change the chord every couple of seconds
+        if (isChordMode)
         {
-            ChordChangeCoroutine();
-            timeToGo = Time.fixedTime + Random.Range(5,10);
+            nextChord = controller.scale.GetRandomChordInScale();
+            ManageSamplesBasedOnNextChord();
         }
     }
-    
-    void ChordChangeCoroutine()
+
+
+    void ManageSamplesBasedOnNextChord()
     {
-        int[] allowedNotes = controller.GetNextChord();
-
-        foreach (int n in allowedNotes)
-        {
-            if (!ItemAlreadyExists(n))
-                LoadFilesWithNote(n);
-        }
-
-        int counter = 0;
-        foreach(int loadedNote in loadedNotes)
-        {
-            Debug.Log("NOTE:" + loadedNote);
-
-            foreach (int allowedNote in allowedNotes)
-            {
-                Debug.Log("ALLOWED NOTE:" + allowedNote);
-
-                if (allowedNote != loadedNote)
-                {
-                    ToggleSoundObject(loadedNote, false);
-                }
-                else
-                {
-                    ToggleSoundObject(loadedNote, true);
-                }
-            }
-        }
-        Debug.Log("MATCHES LEN ->>>>>" + counter);
-
-    }
-
-    void ToggleSoundObject(int value, bool newState)
-    {
-        foreach (KeyValuePair<GameObject, int> pair in soundObjectsDict)
-        {
-            if (EqualityComparer<int>.Default.Equals(pair.Value, value))
-            {
-                //pair.Key.SetActive(newState);
-
-                if (newState)
-                    pair.Key.tag = "Sounds";
-                else
-                    pair.Key.tag = "Unusable";
-            }
-        }
+        // if the note that's inside the chord isn't loaded before, load the corresponding samples
+        // if a note is loaded but it's not in the chord, diactive the corresponding samples
     }
 
     void ScanDirectory()
     {
-        string PATH = "Samples/";
-        
-        DirectoryInfo dir = new DirectoryInfo("Assets/Resources/"+PATH);
-
-        FileInfo[] info = dir.GetFiles("*.wav");
+        dir = new DirectoryInfo("Assets/Resources/" + samplesRootPath);
+        info = dir.GetFiles("*.wav");
 
         foreach (FileInfo f in info)
         {
             // print("Found: " + f.Name);
-            
+
             string[] splittedName = f.Name.Split('-');
 
-            int noteNum = controller.NoteNameToNumber(splittedName[0]);
+            int noteNum = Utility.NoteNameToNumber(splittedName[0]);
 
-            if ((noteNum != -1) && controller.IsInAllowedNotes(noteNum))
+            if ((noteNum != -1) && controller.scale.IsInScaleNotes(noteNum))
             {
-                loadedNotes.Add(noteNum);
-
                 GameObject tmp = Instantiate(audioPrefab);
-                tmp.GetComponent<AudioSource>().clip = Resources.Load<AudioClip>(PATH + f.Name.Replace(".wav", ""));
-                
-                soundObjectsDict.Add(tmp, noteNum);
+                tmp.GetComponent<AudioSource>().clip = Resources.Load<AudioClip>(samplesRootPath + f.Name.Replace(".wav", ""));
 
                 print("LOADED: " + f.Name);
             }
@@ -135,326 +77,189 @@ public class Composer : MonoBehaviour
 
     void LoadFilesWithNote(int noteNumber)
     {
-        string PATH = "Samples/";
-
-        DirectoryInfo dir = new DirectoryInfo("Assets/Resources/" + PATH);
-
-        FileInfo[] info = dir.GetFiles("*.wav");
-
-        foreach (FileInfo f in info)
-        {
-            // print("Found: " + f.Name);
-
-            string[] splittedName = f.Name.Split('-');
-
-            int noteNum = controller.NoteNameToNumber(splittedName[0]);
-
-            if (noteNum == noteNumber)
-            {
-                loadedNotes.Add(noteNum);
-
-                GameObject tmp = Instantiate(audioPrefab);
-                tmp.GetComponent<AudioSource>().clip = Resources.Load<AudioClip>(PATH + f.Name.Replace(".wav", ""));
-                
-                soundObjectsDict.Add(tmp, noteNum);
-
-
-                //print("LOADED ON RUNTIME: " + f.Name);
-            }
-        }
     }
 
-    private bool ItemAlreadyExists(int target)
-    {
-        foreach (int item in loadedNotes)
-        {
-            if (item.Equals(target))
-                return true;
-        }
-        return false;
-    }
 }
 
 class ComposerController
 {
-    
-    private int baseNote;
-    private int scaleType;
-    private int numOctaves;
 
-    private ScaleManager scaleManager;
-    private Dictionary<string, int> noteNames;
+    public Scale scale;
+    private Settings settings;
 
-    public ComposerController(int baseNote, int scaleType, int numOctaves)
+    public ComposerController(int scaleType, int baseNote, int numOctaves)
     {
-        this.baseNote = baseNote;
-        this.scaleType = scaleType;
-        this.numOctaves = numOctaves;
+        settings.scaleType = scaleType;
+        settings.baseNote = baseNote;
+        settings.numOctaves = numOctaves;
 
-        noteNames = new Dictionary<string, int>();
-        InitNoteNamesDict();
-
-        scaleManager = new ScaleManager(this.baseNote, this.scaleType, this.numOctaves);
-    }
-
-    public int[] GetNextChord()
-    {
-        scaleManager.SetRandomChordInScale();
-        scaleManager.CalcAllowedNotes(true);
-        return scaleManager.GetAllowedNotes();
-    }
-
-    public int NoteNameToNumber(string noteName)
-    {
-        if (noteNames.TryGetValue(noteName.ToLower(), out int number))
-        {
-            return number;
-        }
-        return -1;
-    }
-
-    public bool IsInAllowedNotes(int num)
-    {
-        return scaleManager.IsInAllowedScaleNotes(num);
-    }
-
-    public int[] GetAllowedNotes()
-    {
-        return scaleManager.GetAllowedNotes();
-    }
-
-    private void InitNoteNamesDict()
-    {
-        for (int i = 0; i < 8; i++)
-        {
-            noteNames.Add("c" + i.ToString(), 12 * i);
-            noteNames.Add("cs" + i.ToString(), 1 + 12 * i);
-            noteNames.Add("d" + i.ToString(), 2 + 12 * i);
-            noteNames.Add("ds" + i.ToString(), 3 + 12 * i);
-            noteNames.Add("e" + i.ToString(), 4 + 12 * i);
-            noteNames.Add("f" + i.ToString(), 5 + 12 * i);
-            noteNames.Add("fs" + i.ToString(), 6 + 12 * i);
-            noteNames.Add("g" + i.ToString(), 7 + 12 * i);
-            noteNames.Add("gs" + i.ToString(), 8 + 12 * i);
-            noteNames.Add("a" + i.ToString(), 9 + 12 * i);
-            noteNames.Add("as" + i.ToString(), 10 + 12 * i);
-            noteNames.Add("b" + i.ToString(), 11 + 12 * i);
-        }
+        scale = new Scale(settings.scaleType, settings.baseNote, settings.numOctaves);
     }
 }
 
-class ScaleManager
+class Scale
 {
-    int baseNote;
-    int scaleType;
-    int numOctaves;
+    Note[] notes;
+    Chord[] chords;
+    Settings settings;
 
-    private int[] allowedNotes;
-    private int allowedNotesSize;
-
-    private int[] majorScaleNotes;
-    private string[] majorScaleChords;
-
-    private int[] minorScaleNotes;
-    private string[] minorScaleChords;
-
-    private int[] scale;
-    private string[] scaleChords;
-
-
-    public int[] currentChord;
-    private ChordManager chordManager;
-
-    private int currentChordBaseNoteIndex;
-
-
-    public ScaleManager(int baseNote, int scaleType, int numOctaves)
+    public Scale(int type, int baseNote, int numOctaves)
     {
-        this.baseNote = baseNote;
-        this.numOctaves = numOctaves;
-        this.scaleType = scaleType;
+        settings.scaleType = type;
+        settings.baseNote = baseNote;
+        settings.numOctaves = numOctaves;
 
-        majorScaleNotes = new int[7];
-        majorScaleChords = new string[7];
-
-        minorScaleNotes = new int[7];
-        minorScaleChords = new string[7];
-        scaleChords = new string[7];
-
-        InitScales();
-
-        scale = new int[7];
-        SetScaleType();
-
-        CalculateScale(this.baseNote, this.scaleType, this.numOctaves);
-
-        currentChord = new int[3];
-
-        chordManager = new ChordManager();
+        CalculateScaleNotes();
+        CalculateScaleChords();
     }
 
-    public int[] GetAllowedNotes()
+    public bool IsInScaleNotes(int noteNumber)
     {
-        return allowedNotes;
-    }
-
-    public void CalculateScale(int baseNote, int scaleType, int numOctaves)
-    {
-        this.baseNote = baseNote;
-        this.numOctaves = numOctaves;
-        this.scaleType = scaleType;
-        
-        CalcAllowedNotes(false);
-    }
-
-    public void SetRandomChordInScale()
-    {
-        
-        int index = Random.Range(0, 7);
-        currentChordBaseNoteIndex = index;
-
-        Debug.Log("NEW CHORD INDEX::::::" + index);
-        Debug.Log("NEW CHORD TYPE::::::" + scaleChords[index]);
-        string[] notes = chordManager.GetChordNotes(scaleChords[index]);
-
-        int[] result = new int[notes.Length];
-
-        for (int i=0; i< notes.Length; i++)
+        foreach (Note n in notes)
         {
-            if(!notes[i].Contains("b"))
-            {
-                result[i] = int.Parse(notes[i]);
-            }
-            else
-            {
-                result[i] = int.Parse(notes[i].Replace("b",""))-1;
-            }
-        }
-
-        result.CopyTo(currentChord, 0);
-    }
-
-    public void CalcAllowedNotes(bool isChordMode)
-    {
-        int baseVal;
-        int[] notes;
-        if (!isChordMode)
-        {
-            baseVal = 7;
-            notes = new int[7];
-            scale.CopyTo(notes, 0);
-        }
-        else
-        {
-            baseVal = currentChord.Length;
-            //Debug.Log("BASE VAL:::::" + baseVal);
-            notes = new int[baseVal];
-            currentChord.CopyTo(notes, 0);
-        }
-     
-        allowedNotesSize = baseVal * this.numOctaves;
-        allowedNotes = new int[allowedNotesSize];
-
-        int octaveCounter = 0;
-        for (int i = 0; i < allowedNotesSize; i++)
-        {
-            allowedNotes[i] = baseNote + notes[i % baseVal] + 12 * octaveCounter;
-            if (isChordMode)
-                allowedNotes[i] += scale[currentChordBaseNoteIndex];
-
-
-            //Debug.Log("Allowed;;;;" + allowedNotes[i]);
-            if (i % baseVal == baseVal-1) octaveCounter++;
-        }
-    }
-    
-    public bool IsInAllowedScaleNotes(int num)
-    {
-        for (int i = 0; i < allowedNotesSize; i++)
-        {
-            if (allowedNotes[i] == num)
-            {
+            if ((noteNumber % 12) == n.number)
                 return true;
-            }
         }
+
         return false;
     }
 
-    private void SetScaleType()
+    public Note[] GetAllowedNotesInScale()
     {
-        if (this.scaleType == 1)
-        {
-            minorScaleNotes.CopyTo(scale, 0);
-            minorScaleChords.CopyTo(scaleChords, 0);
-        }
-        else if (this.scaleType == 2)
-        {
-            majorScaleNotes.CopyTo(scale, 0);
-            majorScaleChords.CopyTo(scaleChords, 0);
-
-        }
+        return Utility.ExpandNotesIntoOctaves(notes, settings.baseNote, settings.numOctaves);
     }
 
-    private void InitScales()
+    public Note[] GetRandomChordInScale()
     {
-        majorScaleNotes[0] = 0;
-        majorScaleNotes[1] = 2;
-        majorScaleNotes[2] = 4;
-        majorScaleNotes[3] = 5;
-        majorScaleNotes[4] = 7;
-        majorScaleNotes[5] = 9;
-        majorScaleNotes[6] = 11;
+        return chords[Random.Range(0, chords.Length)].GetChordNotes(settings.baseNote, settings.numOctaves);
+    }
 
-        minorScaleNotes[0] = 0;
-        minorScaleNotes[1] = 2;
-        minorScaleNotes[2] = 3;
-        minorScaleNotes[3] = 5;
-        minorScaleNotes[4] = 7;
-        minorScaleNotes[5] = 8;
-        minorScaleNotes[6] = 10;
+    public void CalculateScaleNotes()
+    {
+        notes = Utility.GetScaleNotes(settings.scaleType);
+    }
 
-        majorScaleChords[0] = "M";
-        majorScaleChords[1] = "m";
-        majorScaleChords[2] = "m";
-        majorScaleChords[3] = "M";
-        majorScaleChords[4] = "M";
-        majorScaleChords[5] = "m";
-        majorScaleChords[6] = "dim";
+    public void CalculateScaleChords()
+    {
 
-        minorScaleChords[0] = "m";
-        minorScaleChords[1] = "dim";
-        minorScaleChords[2] = "M";
-        minorScaleChords[3] = "m";
-        minorScaleChords[4] = "m";
-        minorScaleChords[5] = "M";
-        minorScaleChords[6] = "M";
     }
 }
 
-class ChordManager
+class Chord
 {
-    private Dictionary<string, string[]> chordTypes;
+    Note[] notes;
 
-    public ChordManager()
+    public Chord()
     {
-        chordTypes = new Dictionary<string, string[]>();
-        InitChordTypes();
+
     }
 
-    public void InitChordTypes()
-    { 
-        chordTypes.Add("M", new string [3] { "0","2","4" });
-        chordTypes.Add("m", new string[3] { "0", "2", "4" });
-        chordTypes.Add("dim", new string[3] { "0", "2", "4b" });
+    public Note[] GetNotes()
+    {
+        return notes;
     }
 
-    public string[] GetChordNotes(string name)
+    public bool IsInChordNotes(int noteIndex)
     {
-        if (chordTypes.TryGetValue(name, out string[] result))
+        foreach (Note n in notes)
         {
-            return result;
+            if (noteIndex == n.number)
+                return true;
         }
-        return null;
-        
+
+        return false;
+    }
+
+    public Note[] GetChordNotes(int baseNote, int numOctaves)
+    {
+        return Utility.ExpandNotesIntoOctaves(notes, baseNote, numOctaves);
+    }
+}
+
+class Note
+{
+    public int number;
+    public string name;
+
+    public Note(int n)
+    {
+        number = n;
+    }
+
+}
+
+static class Utility
+{
+    public static Note[] ExpandNotesIntoOctaves(Note[] notes, int baseNote, int numOctaves)
+    {
+        int size = notes.Length * numOctaves;
+        Note[] result = new Note[size];
+
+        for (int i = 0; i < size; i++)
+        {
+            result[i] = new Note(baseNote + notes[i % notes.Length].number + (i % 12));
+        }
+
+        return result;
+    }
+
+    public static Note[] GetScaleNotes(int type)
+    {
+        Note[] notes = new Note[7];
+        if (type == 1)
+        {
+            notes[0] = new Note(0);
+            notes[1] = new Note(2);
+            notes[2] = new Note(3);
+            notes[3] = new Note(5);
+            notes[4] = new Note(7);
+            notes[5] = new Note(8);
+            notes[6] = new Note(10);
+        }
+
+        else if (type == 2)
+        {
+            notes[0] = new Note(0);
+            notes[1] = new Note(2);
+            notes[2] = new Note(4);
+            notes[3] = new Note(5);
+            notes[4] = new Note(7);
+            notes[5] = new Note(9);
+            notes[6] = new Note(11);
+        }
+
+        return notes;
+    }
+
+    public static int NoteNameToNumber(string name)
+    {
+        // It CAN be done with dictionary pretty simply.
+
+        //  a  /  as  /  b  /  c  /  cs  /  d  /  ds  /  e  /  f  /  fs  /  g  /  gs  /
+
+        int length = name.Length;
+
+        int octave = int.Parse(name[length - 1].ToString());
+
+        int result = 0;
+        if (length == 3) // aka it's a # note (Because the name will be like As4 with the length of 3 chars.)
+        {
+            result = +1;
+        }
+
+        switch (name[0])
+        {
+            case 'c': result += 0; break;
+            case 'd': result += 2; break;
+            case 'e': result += 4; break;
+            case 'f': result += 5; break;
+            case 'g': result += 7; break;
+            case 'a': result += 9; break;
+            case 'b': result += 11; break;
+        }
+
+        result += 12 * octave;
+
+        return result;
     }
 }
